778d65bce89752974ec4e1291834552ca73b189d
Florestan Lefevre <florestanlefevre@gmail.com>
2018-03-07 14:47:53 -0800

    Create fish object from form input values

--- catch-of-the-day/src/components/AddFormFish.js
+++ catch-of-the-day/src/components/AddFormFish.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
 class AddFormFish extends React.Component {
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = (e) => {
+    // 1. prevent default refresh after submitting
+    e.preventDefault();
+    // 2. create fish object
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    }
+    // 3. pass fish object to parent state
+  }
+
   render() {
     return (
-      <form className='fish-edit'>
-        <input type="text" name='name' placeholder='Name'/>
-        <input type="text" name='price' placeholder='Price'/>
-        <select name="status">
+      <form className='fish-edit' onSubmit={this.createFish}>
+        <input type="text" name='name' placeholder='Name' ref={this.nameRef}/>
+        <input type="text" name='price' placeholder='Price' ref={this.priceRef}/>
+        <select name="status" ref={this.statusRef}>
           <option value="available">Fresh</option>
           <option value="unavailable">Sold out</option>
         </select>
-        <textarea type="text" name='desc' placeholder='Desc'/>
-        <input type="text" name='image' placeholder='Image'/>
+        <textarea type="text" name='desc' placeholder='Desc' ref={this.descRef}/>
+        <input type="text" name='image' placeholder='Image' ref={this.imageRef}/>
         <button type='submit'>+ item</button>
       </form>
     );
