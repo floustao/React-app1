f36ed72d39e3d36e35ea2902994d7a902c6328b2
Florestan Lefevre <florestanlefevre@gmail.com>
2018-03-24 22:44:45 -0700

    Update order state using addToOrder and props to access key from Fish component

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,21 +2,26 @@ import React from 'react';
 import {formatPrice} from '../helpers';
 
 class Fish extends React.Component {
+  handleClick = () => {
+    // run addToOrder function which requires a key
+    this.props.addToOrder(this.props.index);
+  }
   render() {
-    const { image, name, price, desc } = this.props.details;
+    const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
     return (
-      <div className="single-fish">
-        <li className="menu-fish">
-          <img src={image} alt={name}/>
-          <h3 className="fish-name">
-            {name}
-            <span className="price">{formatPrice(price)}</span>
-          </h3>
-          <p>{desc}</p>
-          <button>Add to cart</button>
-        </li>
-      </div>
-    )
+      <li className="menu-fish">
+        <img src={image} alt={name}/>
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? 'Add to cart' : 'Sold Out'}
+        </button>
+      </li>
+    );
   }
 }
 
