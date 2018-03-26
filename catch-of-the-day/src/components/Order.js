6ef2770d4ea4d0d52fb991231df2aa43fa06b5dd
Florestan Lefevre <florestanlefevre@gmail.com>
2018-03-25 19:00:12 -0700

    Update total order price after adding fish to cart if fish is available

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, i) => {
+      const count = this.props.order[i];
+      const fish = this.props.fishes[i];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        prevTotal += count * fish.price
+      }
+      return prevTotal
+    }, 0);
+
     return (
       <div className="order">
         <h2>Order</h2>
+        {formatPrice(total)}
       </div>
     )
   }
